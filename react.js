44fe193d2fd5a5372e0d387a67f7f9b6289a485a
Adi <77723061+adishgarg@users.noreply.github.com>
2024-01-01 13:11:24 +0530

    initialize

---
+++ react.js
@@ -0,0 +1,11 @@
+function App(){
+    return(
+        <>
+        <h1>Chat-Bot</h1>
+        </>
+    )
+}
+
+const renderElement = ReactDOM.createRoot(document.getElementById("root"))
+
+renderElement.render(<App />)
